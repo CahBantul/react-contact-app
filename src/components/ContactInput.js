f922a7e094565388644b7d82ef719e003e0db178
fardan <fardan.nozami@gmail.com>
2022-06-16 20:40:48 +0700

    add component contactinput

---
+++ src/components/ContactInput.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi state
+    this.state = {
+      name: '',
+      tag: '',
+    };
+  }
+
+  onNameChangeEventHandler = (event) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        name: event.target.value,
+      };
+    });
+  };
+
+  onNameTagEventHandler = (event) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        tag: event.target.value,
+      };
+    });
+  };
+
+  onSubmitEventHandler = (event) => {
+    event.preventDefault();
+    this.props.addContact(this.state);
+  };
+
+  render() {
+    return (
+      <form className="contact-input">
+        <input type="text" placeholder="Nama" value={this.state.name} />
+        <input type="text" placeholder="Tag" value={this.state.tag} />
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
+}
+
+export default ContactInput;
